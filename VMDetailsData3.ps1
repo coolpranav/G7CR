02740a3f578693b3939791c3481edab9402beddf
Pranav Kumar <30388181+coolpranav@users.noreply.github.com>
2020-05-29 06:16:55 +0530

    Add files via upload

---
+++ VMDetailsData3.ps1
@@ -0,0 +1,9 @@
+﻿$POVMs = Get-AzVM  | Where-Object {$VM.HardwareProfile.VmSize -inotlike 'Standard_B2s'}
+$POVMs | Sort
+
+$POVMs = Get-AzVM | Where-Object {$env:ALLUSERSPROFILE} | Where-Object {$VM.HardwareProfile -notlike 'Standard_B'}
+
+$Hello = Get-AzVM
+$Hello | ft ResourceGroupName,Name,
+
+Get-AzVM | Where-Object {$env:ALLUSERSPROFILE}| ft Name,ResourceGroupName,{$VM.HardwareProfile.VmSize} | Export-FormatData C:\Users\User\Desktop\Nello
