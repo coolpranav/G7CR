d7b248c8171d2e734affb82a09e339e58bbef9bc
Pranav Kumar <30388181+coolpranav@users.noreply.github.com>
2021-04-08 07:33:42 +0530

    Update Metricsreportfrompowershell.ps1

--- Metricsreportfrompowershell.ps1
+++ Metricsreportfrompowershell.ps1
@@ -1,19 +1,19 @@
 Login-AzAccount
 mkdir -P D:\CLIENTNAME\
 #Put your all VM name and the Resource group where it belongs to one CSV and you can do it from portal i.e., Export to CSV.
-foreach($VM in import-csv "D:\AG7\AG7\Clients\VMukti\Data3.csv")
+foreach($VM in import-csv "D:\Clients\Data3.csv")
 {
 $n= $VM.Name
 $n
 mkdir $n
 $m= $VM.RESOURCEGROUP
-$Metric=Get-AzMetric  -ResourceId /subscriptions/a56fd836-5457-4289-8ed5-adfb8b655bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName  'Percentage CPU' -AggregationType Maximum -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
-$Metric1=Get-AzMetric -ResourceId /subscriptions/a56fd836-5457-4289-8ed5-adfb8b655bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Network In Total' -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
-$Metric2=Get-AzMetric -ResourceId /subscriptions/a56fd836-5457-4289-8ed5-adfb8b655bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Network Out Total' -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
-$Metric3=Get-AzMetric -ResourceId /subscriptions/a56fd836-5457-4289-8ed5-adfb8b655bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Disk Read Bytes' -AggregationType Maximum -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
-$Metric4=Get-AzMetric -ResourceId /subscriptions/a56fd836-5457-4289-8ed5-adfb8b655bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Disk Write Bytes' -AggregationType Maximum -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
-$Metric5=Get-AzMetric -ResourceId /subscriptions/a56fd836-5457-4289-8ed5-adfb8b655bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Disk Read Operations/Sec' -AggregationType Maximum -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
-$Metric6=Get-AzMetric -ResourceId /subscriptions/a56fd836-5457-4289-8ed5-adfb8b655bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Disk Write Operations/Sec' -AggregationType Maximum -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
+$Metric=Get-AzMetric  -ResourceId /subscriptions/l56fd102-5457-4401-8ed5-adfb8b210bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName  'Percentage CPU' -AggregationType Maximum -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
+$Metric1=Get-AzMetric -ResourceId /subscriptions/l56fd102-5457-4401-8ed5-adfb8b210bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Network In Total' -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
+$Metric2=Get-AzMetric -ResourceId /subscriptions/l56fd102-5457-4401-8ed5-adfb8b210bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Network Out Total' -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
+$Metric3=Get-AzMetric -ResourceId /subscriptions/l56fd102-5457-4401-8ed5-adfb8b210bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Disk Read Bytes' -AggregationType Maximum -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
+$Metric4=Get-AzMetric -ResourceId /subscriptions/l56fd102-5457-4401-8ed5-adfb8b210bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Disk Write Bytes' -AggregationType Maximum -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
+$Metric5=Get-AzMetric -ResourceId /subscriptions/l56fd102-5457-4401-8ed5-adfb8b210bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Disk Read Operations/Sec' -AggregationType Maximum -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
+$Metric6=Get-AzMetric -ResourceId /subscriptions/l56fd102-5457-4401-8ed5-adfb8b210bfc/resourceGroups/$m/providers/Microsoft.Compute/virtualMachines/$n -MetricName 'Disk Write Operations/Sec' -AggregationType Maximum -StartTime 2021-04-06T00:32:00Z -EndTime 2021-04-06T16:37:00Z -TimeGrain 00:05:00
 
 $Metric.Data | Export-Csv  D:\CLIENTNAME\$n\PERCENTAGECPU.csv
 $Metric1.Data | Export-Csv D:\CLIENTNAME\$n\NETWORKINTOTAL.csv
