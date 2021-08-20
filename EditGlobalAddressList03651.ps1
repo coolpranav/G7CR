1939a070d7d4836b11f7c9c6e31c63b4505d05f3
Pranav Kumar <30388181+coolpranav@users.noreply.github.com>
2021-08-21 03:24:17 +0530

    Create EditGlobalAddressList03651.ps1

---
+++ EditGlobalAddressList03651.ps1
@@ -0,0 +1,11 @@
+Set-ExecutionPolicy RemoteSigned
+Install-Module -Name ExchangeOnlineManagement
+Connect-ExchangeOnline
+Set-User -Identity g7support@metriqe.com -Manager sumitg@metriqe.com
+
+$csvFileLocation = "C:\Users\Pranav Kumar\Desktop\OneDrive.csv"
+$users = Import-Csv -Path $csvFileLocation
+foreach ($user in $users) {$upn = $user.'Identity'; $manager = $user.'Manager';
+
+Set-User -Identity $upn -Manager $manager 
+}
